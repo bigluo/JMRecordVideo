1121408f18c2b20f71b683728db3490bbb98c368
bigluo <654298473@qq.com>
2018-07-21 16:19:24 +0800

    “”

---
+++ RecordVideoExample/JMVideoCaptureHelper.h
@@ -0,0 +1,13 @@
+//
+//  JMVideoCaptureHelper.h
+//  RecordVideoExample
+//
+//  Created by 123 on 2018/7/21.
+//  Copyright © 2018年 JM. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMVideoCaptureHelper : NSObject
+
+@end
---
+++ RecordVideoExample/JMVideoCaptureHelper.m
@@ -0,0 +1,13 @@
+//
+//  JMVideoCaptureHelper.m
+//  RecordVideoExample
+//
+//  Created by 123 on 2018/7/21.
+//  Copyright © 2018年 JM. All rights reserved.
+//
+
+#import "JMVideoCaptureHelper.h"
+
+@implementation JMVideoCaptureHelper
+
+@end
